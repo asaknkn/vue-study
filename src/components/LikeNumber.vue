cc9d3a34a1ca2024bd91de33a98c01eecdf49226
Kentarou Asai <asaknkn@gmail.com>
2021-08-14 16:11:38 +0900

    use props and emit

--- src/components/LikeNumber.vue
+++ src/components/LikeNumber.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <p>いいね: {{number}}</p>
+    <p>いいね: {{halfNumber}}</p>
     <button @click="increment">+1</button>
   </div>
   
@@ -9,14 +9,20 @@
 <script>
 
 export default {
-  data() {
-    return {
-      number: 5
+  props: {
+    totalNumber: {
+      type: Number,
+      default: 10
+    }
+  },
+  computed: {
+    halfNumber() {
+      return this.totalNumber /2
     }
   },
   methods: {
     increment() {
-      this.number += 1
+      this.$emit("my-click", this.totalNumber + 1)
     }
   }
 }
