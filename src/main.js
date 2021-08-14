de476ece7893e1fb6c01408e9cae0fd89d261a55
Kentarou Asai <asaknkn@gmail.com>
2021-08-14 14:33:52 +0900

    use components

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue' // vue ファイルは import すると コンポーネントとなる
+import LikeNumber from "./components/LikeNumber"
 
 Vue.config.productionTip = false
+Vue.component("LikeNumber", LikeNumber)
 
 new Vue({
   render: h => h(App), //コンポーネントのオブジェクトが App 
