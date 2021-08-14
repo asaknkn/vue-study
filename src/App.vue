7057c7085ce670911c9e5360f9beea834977c527
Kentarou Asai <asaknkn@gmail.com>
2021-08-14 17:41:48 +0900

    use keep-alive

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,36 @@
 <template>
   <div>
-    <LikeHeader header-text="===========================">
-      <template v-slot:title>
-        <h1>トータルのいいね数</h1>
-      </template>
-      <h2>皆さん</h2>
-      <h3>はじまして</h3>
-      <template v-slot:number>
-        <h2>{{number}}</h2>
-      </template>
-      <template #[title]></template>
-      
+    <LikeHeader>
+      <h3>初めまして</h3>
     </LikeHeader>
    
     <LikeNumber :total-number="number" @my-click="incrementNumber"></LikeNumber>
-    <LikeNumber :total-number="number"></LikeNumber>
+
+    <button @click="currentComponent = 'Home'">Home</button>
+    <button @click="currentComponent = 'About'">About</button>
+    <keep-alive>
+      <component :is="currentComponent"></component> <!-- keep-alive がないと切り替わるたびにコンポーネントが destroyed される -->
+    </keep-alive>
+    
   </div>
 </template>
 
 <script>
 import LikeHeader from "./components/LikeHeader.vue"
+import About from "./components/About.vue"
+import Home from "./components/Home.vue"
 
   export default {
     data() {
       return {
         number: 14,
-        title: "titleee"
+        currentComponent: "Home"
       }
     },
     components: {
-      LikeHeader
+      LikeHeader,
+      About,
+      Home
     },
     methods: {
       incrementNumber(value) {
--- src/components/LikeHeader.vue
+++ src/components/LikeHeader.vue
@@ -1,15 +1,6 @@
 <template>
   <div>
     <slot></slot>
-    <slot name="title">
-      <h2>デフォルトのタイトル</h2>
-    </slot>
-    <p>{{headerText}}</p>
-    <p>いいねの数</p>
-    <slot name="number"></slot>
-    <slot name="titleee">
-      <h2>デフォルトのタイトル2</h2>
-    </slot>
   </div>
 </template>
 
