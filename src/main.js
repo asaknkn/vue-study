5ef2f9f917fc6b01173196641d763d432747f57b
Kentarou Asai <asaknkn@gmail.com>
2021-08-15 18:02:07 +0900

    use vuex

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue' // vue ファイルは import すると コンポーネントとなる
 import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
-  router: router,
+  router,
+  store,
   render: h => h(App), //コンポーネントのオブジェクトが App 
 }).$mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,37 @@
   <div>
     <h3>Home</h3>
     <button @click="toUsers">Usersのページに行く</button>
+    <p>{{doubleCount}}</p>
+    <p>{{tripleCount}}</p>
+    <input 
+      type="text"
+      v-model="message"
+    >
+    <p>{{message}}</p>
   </div>
 </template>
 
 <script>
+import {mapGetters} from 'vuex'
 export default {
+  
+  computed: {
+    ...mapGetters("count",["doubleCount", "tripleCount"]),
+    message: {
+      get() {
+        return this.$store.getters.message
+      },
+      set(value) {
+        this.$store.dispatch("updateMessage", value)
+      }
+    }
+  },
+  /*
+  computed: mapGetters({
+    doubleCount: "doubleCount",
+    tripleCount: "tripleCount"
+  }),
+  */
   methods: {
     toUsers() {
       this.$router.push({
